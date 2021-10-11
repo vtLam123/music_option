b20144f61bfe487a810f6823745ec4723f0f52b4
Vo Thanh Lam <vtlam20@clc.fiftus.edu.vn>
2021-10-11 15:00:32 +0700

    first commit

---
+++ music/music.cpp
@@ -0,0 +1,11 @@
+﻿#include <windows.h>
+#include <Mmsystem.h>
+#include <mciapi.h>
+#pragma comment(lib, "Winmm.lib")
+
+int main()
+{
+    mciSendString(TEXT("open \"abc.mp3\" type mpegvideo alias mp3"), NULL, 0, NULL); //hàm mở file mp3
+    mciSendString(TEXT("play mp3 repeat"), NULL, 0, NULL);      //hàm chơi repeat nhạc
+    system("pause");
+}
